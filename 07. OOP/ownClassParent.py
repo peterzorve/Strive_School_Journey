5d7f49d7c53735b33f2b089183273c130d2caa39
peterzorve <peterzorve28@gmail.com>
2022-02-03 15:50:44 +0200

    own_class_parent
    OWN CLASS PARENT

---
+++ 07. OOP/ownClassParent.py
@@ -0,0 +1,19 @@
+x = 10
+import math
+
+class MathOperations:
+    def __init__(self, firstNumber, secondNumber):
+        self.firstNumber = firstNumber
+        self.secondNumber = secondNumber
+
+    def summation(self):
+        return self.firstNumber + self.secondNumber
+
+    def difference(self):
+        return self.firstNumber - self.secondNumber    
+
+    def multiplication(self):
+        return self.firstNumber * self.secondNumber
+
+    def division(self):
+        return self.firstNumber / self.secondNumber
